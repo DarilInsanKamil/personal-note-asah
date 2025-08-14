dbd5185a991215211216cc825854b3bee8d4d344
DarilInsanKamil <darilinsankamil2132@gmail.com>
2025-08-14 11:42:29 +0700

    feat: add NotFound page and style for 404 error handling

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ArchivePage from './page/ArchivePage';
 import AddPage from './page/AddPage';
 import DetailNotePage from './page/DetailNotePage';
 import EditNotePage from './page/EditNotePage';
+import NotFound from './page/NotFound';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/add-note' element={<AddPage />} />
         <Route path='/detail/:id' element={<DetailNotePage />} />
         <Route path='/edit-note/:id' element={<EditNotePage />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
